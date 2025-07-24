0709979c60c16fe2ee73ec96265df7f8d7bbb3c0
JoergZ2 <joerg.zywek@gmx.de>
2025-07-24 09:17:56 +0200

    Update 20250724 09:17

--- __init__.py
+++ __init__.py
@@ -181,7 +181,8 @@ class MyGermanPublicApi(OVOSSkill):
                     warn_type = data['data'][i]['lhpClassName']
                     warn_area = data['data'][i]['areaDesc']
                     self.speak("Region: " + warn_area + ", Warnungsart: " + warn_type)
-                    sleep(0.5)
+                    if len(data['data']) > 1 and len(data['data'] - [i] > 0:
+                        sleep(3)
                     i += 1
             else:
                 self.speak("Aktuell liegen keine Hochwasserdaten vor.")
